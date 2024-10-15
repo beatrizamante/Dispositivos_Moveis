74588ca06e69e41bd9bf222bf193f9350fca7e06
Beatriz <beatrizamante@hotmail.com>
2024-10-15 19:17:29 -0300

    Atividade5_useState

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './components/Login';
+
+
+export default function App() {
+  return (
+    <View >
+      <Login />
+    </View>
+  );
+}
+
+
---
+++ components/Login.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { ScrollView, StyleSheet, TextInput, Button } from "react-native";
+
+export default function Feed() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const handleLogin = console.log(`Username ${username} and password ${password}`)
+
+    return (
+        <ScrollView>
+            <TextInput
+                style={styles.input}
+                value={username}
+                onChangeText={(val) => setUsername(username)}
+                placeholder="username"
+            >
+                {username}
+            </TextInput>
+            
+            <TextInput
+                style={styles.input}
+                value={password}
+                onChangeText={(val) => setPassword(password)}
+                secureTextEntry={true}
+                placeholder="password"
+            >
+                {password}
+            </TextInput>
+            
+            <Button
+                title="Submit"
+                onPress={() => handleLogin}
+            />
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+});
